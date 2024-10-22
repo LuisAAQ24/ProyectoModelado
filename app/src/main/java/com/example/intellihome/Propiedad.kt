4368ce00f5cc9dcf1632f92ac51290f4b3b2919e
Luis Aguero <l.aguero.2@estudiantec.cr>
2024-10-22 01:36:47 -0600

    hk

---
+++ app/src/main/java/com/example/intellihome/Propiedad.kt
@@ -0,0 +1,12 @@
+package com.example.intellihome
+
+data class Propiedad(
+    val id: String,
+    val nombre: String,
+    val ubicacion: String,
+    val precio: Double,
+    val descripcion: String,
+    val imagenUrl: String,
+    val colorFondo: Int
+
+)
